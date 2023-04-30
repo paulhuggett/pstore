429045b0143407ef8060f1de21ac1f3cedb65320
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-04-30 11:30:28 +0200

    std::array<> iterators are not necessarily pointers.

--- include/pstore/serialize/standard_types.hpp
+++ include/pstore/serialize/standard_types.hpp
@@ -62,7 +62,8 @@ namespace pstore::serialize {
         *(last++) = 0;
       }
       // Emit the string length.
-      auto const resl = serialize::write (archive, gsl::make_span (first, last));
+      auto const resl = serialize::write (
+        archive, gsl::make_span (std::addressof (*first), std::addressof (*last)));
 
       // Emit the string body.
       serialize::write (std::forward<Archive> (archive), gsl::make_span (str));
