f05052ffe4e70c94b27217e58d8e156b5fb50c98
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-05-13 11:11:06 +0200

    Separate the erase() return value from the expected result.

--- unittests/adt/test_small_vector.cpp
+++ unittests/adt/test_small_vector.cpp
@@ -343,49 +343,58 @@ TYPED_TEST_SUITE (SmallVectorErase, Sizes, );
 // NOLINTNEXTLINE
 TYPED_TEST (SmallVectorErase, SinglePos) {
   pstore::small_vector<int, TypeParam::value> v{1, 2, 3};
-  EXPECT_EQ (v.erase (v.cbegin ()), v.begin());
+  auto const l1 = v.erase (v.cbegin ());
+  EXPECT_EQ (l1, v.begin ());
   EXPECT_THAT (v, testing::ElementsAre (2, 3));
-  EXPECT_EQ (v.erase (v.cbegin ()), v.begin());
+  auto const l2 = v.erase (v.cbegin ());
+  EXPECT_EQ (l2, v.begin ());
   EXPECT_THAT (v, testing::ElementsAre (3));
-  EXPECT_EQ (v.erase (v.cbegin ()), v.begin());
+  auto const l3 = v.erase (v.cbegin ());
+  EXPECT_EQ (l3, v.begin ());
   EXPECT_TRUE (v.empty ());
 }
 // NOLINTNEXTLINE
 TYPED_TEST (SmallVectorErase, SingleSecondElement) {
   pstore::small_vector<int, TypeParam::value> v{1, 2, 3};
-  EXPECT_EQ (v.erase (v.begin () + 1), v.begin () + 1);
+  auto const last = v.erase (v.begin () + 1);
+  EXPECT_EQ (last, v.begin () + 1);
   EXPECT_THAT (v, testing::ElementsAre (1, 3));
 }
 // NOLINTNEXTLINE
 TYPED_TEST (SmallVectorErase, SingleFinalElement) {
   pstore::small_vector<int, TypeParam::value> v{1, 2, 3};
-  EXPECT_EQ (v.erase (v.begin () + 2), v.begin () + 2);
+  auto const last = v.erase (v.begin () + 2);
+  EXPECT_EQ (last, v.begin () + 2);
   EXPECT_THAT (v, testing::ElementsAre (1, 2));
 }
 // NOLINTNEXTLINE
 TYPED_TEST (SmallVectorErase, RangeAll) {
   pstore::small_vector<int, TypeParam::value> a{1, 2, 3};
-  EXPECT_EQ (a.erase (a.begin (), a.end ()), a.end());
+  auto const last = a.erase (a.begin (), a.end ());
+  EXPECT_EQ (last, a.end ());
   EXPECT_TRUE (a.empty ());
 }
 // NOLINTNEXTLINE
 TYPED_TEST(SmallVectorErase, RangeFirstTwo) {
   pstore::small_vector<int, TypeParam::value> b{1, 2, 3};
   auto const first = b.begin ();
-  EXPECT_EQ (b.erase (first, first + 2), first);
+  auto const last = b.erase (first, first + 2);
+  EXPECT_EQ (last, first);
   EXPECT_THAT (b, testing::ElementsAre (3));
 }
 // NOLINTNEXTLINE
 TYPED_TEST(SmallVectorErase, RangeFirstOnly) {
   pstore::small_vector<int, TypeParam::value> b{1, 2, 3};
   auto const first = b.begin ();
-  EXPECT_EQ (b.erase (first, first + 1), first);
+  auto const last = b.erase (first, first + 1);
+  EXPECT_EQ (last, first);
   EXPECT_THAT (b, testing::ElementsAre (2, 3));
 }
 // NOLINTNEXTLINE
 TYPED_TEST(SmallVectorErase, RangeSecondToEnd) {
   pstore::small_vector<int, TypeParam::value> b{1, 2, 3};
   auto const first = b.begin () + 1;
-  EXPECT_EQ (b.erase (first, b.end ()), first);
+  auto const last = b.erase (first, b.end ());
+  EXPECT_EQ (last, first);
   EXPECT_THAT (b, testing::ElementsAre (1));
 }
